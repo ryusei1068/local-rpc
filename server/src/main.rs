28de5df0107a0ce5e95c2e9120948bbcb824f61c
Ryusei Nohara <nhrryi@gmail.com>
2023-03-25 21:47:41 +0900

    valid anagram method & test code

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,9 +5,9 @@ use async_std::task;
 use fake::faker::name::raw::*;
 use fake::locales::*;
 use fake::Fake;
+use std::collections::HashMap;
 use std::fs;
 use std::path::Path;
-
 
 fn floor(x: f64) -> f64 {
     x.floor()
@@ -21,6 +21,18 @@ fn reverse(s: String) -> String {
     s.chars().rev().collect::<String>()
 }
 
+fn valid_anagram(s: String, t: String) -> bool {
+    char_counts(&s) == char_counts(&t)
+}
+
+fn char_counts(s: &String) -> HashMap<char, u32> {
+    let mut map = HashMap::new();
+    for c in s.chars() {
+        map.entry(c).and_modify(|num| *num += 1).or_insert(1);
+    }
+
+    map
+}
 
 async fn run() -> std::io::Result<()> {
     let socket_path = "/socket_file";
@@ -74,4 +86,13 @@ fn test_nroot() {
 #[test]
 fn test_reverse() {
     assert_eq!("olleh", reverse("hello".to_string()));
+}
+
+#[test]
+fn test_valid_anagram() {
+    assert_eq!(false, valid_anagram("rat".to_string(), "car".to_string()));
+    assert_eq!(
+        true,
+        valid_anagram("anagram".to_string(), "nagaram".to_string())
+    );
 }
