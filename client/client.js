8d643c8a0b16c997b1a0394a1c572c69be27c0ee
Ryusei Nohara <nhrryi@gmail.com>
2023-03-26 14:12:09 +0900

    timeout

--- client/client.js
+++ client/client.js
@@ -1,9 +1,20 @@
 const net = require('net');
 
 const socket_path = "/socket_file";
-const client = net.createConnection(socket_path);
 
-client.on('connect', () => {
-    console.log('connected.');
-    client.write("hello");
-});
+const client = net.createConnection(socket_path, () => {
+        console.log('connected to server!');
+        client.write('world!\r\n');
+    });
+    client.on('data', (data) => {
+        console.log(data.toString());
+        client.end();
+    });
+    client.on('end', () => {
+        console.log('disconnected from server');
+    });
+    client.on('timeout', () => {
+        console.log('socket timeout');
+        client.end();
+    });
+client.setTimeout(3000);
